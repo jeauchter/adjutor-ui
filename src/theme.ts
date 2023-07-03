b90ab2d8865e17435b385231cfde85e823521e16
Jeremy Auchter <auchterj@gmail.com>
2023-07-03 08:57:12 -0400

    first commitr

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#546e7a',
+    },
+    secondary: {
+      main: '#a5d6a7',
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+});
+
+export default theme;
+
